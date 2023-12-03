856db9045a6ecddaa3fba28660d82f333bb5a69e
Joan M.H <73333828+JMayHub@users.noreply.github.com>
2023-12-03 13:05:56 +0100

    Version 1.1
    Documentation added to Scripts.

--- Assets/Scripts/ItemsController.cs
+++ Assets/Scripts/ItemsController.cs
@@ -18,6 +18,7 @@ public class ItemsController : MonoBehaviour
         PrepareItems();
     }
 
+    //Generate a list of items which will be used as reference points to help.
     void PrepareItems()
     {
         GameObject parent = GameObject.Find("Items");
@@ -35,29 +36,34 @@ public class ItemsController : MonoBehaviour
     {
         timeElapsed += Time.deltaTime;
 
+        //Main condition to generate an item of reference with space key.
         if (timeElapsed > spawnTime && Input.GetKey("space"))
         {
             SpawnItem();
         }
     }
 
+    //Manage the number of items generated to avoid infinity items of reference.
     void SpawnItem()
     {
         timeElapsed = 0f;
         SetItem();
         itemsCount++;
 
+        //Restart item list position
         if (itemsCount == poolSize)
         {
             itemsCount = 0;
         }
     }
 
+    //Get player position to set an item reference on the same position.
     void SetItem()
     {
         Vector3 spawnPosition = gameObject.GetComponent<Rigidbody>().position;
         items[itemsCount].transform.position = spawnPosition;
 
+        //Activate items of reference on the first loop list.
         if (!items[itemsCount].activeSelf)
         {
             items[itemsCount].SetActive(true);
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,21 +14,25 @@ public class PlayerController : MonoBehaviour
         Move(VerticalController());
     }
 
+    //Player rotation with the specified direction and rotation speed.
     void Rotate(float rotation)
     {
         transform.Rotate(new Vector3(0, rotation, 0) * rotationSpeed);
     }
 
+    //Get Axis from the horizontal player control.  
     float HorizontalController()
     {
         return Input.GetAxis("Horizontal");
     }
 
+    //Set a new movement for the player specified by the controller and movement speed.
     void Move(float movement)
     {
         gameObject.GetComponent<CharacterController>().Move(transform.TransformDirection(new Vector3(0, 0, movement) * movementSpeed * Time.deltaTime));
     }
 
+    //Get Axis from the vertical player control. 
     float VerticalController()
     {
         return Input.GetAxis("Vertical");
